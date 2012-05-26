3bd4c02c0b2df3e998b226206f6e989f9a39e1b9
Richard <richard@memespring.co.uk>
2012-05-26 12:50:47 +0100

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
