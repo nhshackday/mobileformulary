1a0ad1ddf250828194c786bce5acb22df2fe655b
Richard <richard@memespring.co.uk>
2012-05-26 15:41:30 +0100

    basic templates

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
-
-
 @app.route("/")
 def index():
     return render_template('index.html')
+    
+@app.route("/search")
+def index():
+    return render_template('search.html')
 
+@app.route("/result")
+def index():
+    return render_template('result.html')
+    
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
