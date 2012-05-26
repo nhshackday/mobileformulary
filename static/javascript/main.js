5bcaca7f27971a35e517934ac4a0e2450b73010a
Richard <richard@memespring.co.uk>
2012-05-26 14:34:05 +0100

    bubble

---
+++ static/javascript/main.js
@@ -0,0 +1,40 @@
+//setup iphone bookmark prompt
+ window.addEventListener('load', function() {
+   window.setTimeout(function() {
+     var bubble = new google.bookmarkbubble.Bubble();
+
+     var parameter = 'bmb=1';
+
+     bubble.hasHashParameter = function() {
+       return window.location.hash.indexOf(parameter) != -1;
+     };
+
+     bubble.setHashParameter = function() {
+       if (!this.hasHashParameter()) {
+         window.location.hash += parameter;
+       }
+     };
+
+     bubble.getViewportHeight = function() {
+       window.console.log('Example of how to override getViewportHeight.');
+       return window.innerHeight;
+     };
+
+     bubble.getViewportScrollY = function() {
+       window.console.log('Example of how to override getViewportScrollY.');
+       return window.pageYOffset;
+     };
+
+     bubble.registerScrollHandler = function(handler) {
+       window.console.log('Example of how to override registerScrollHandler.');
+       window.addEventListener('scroll', handler, false);
+     };
+
+     bubble.deregisterScrollHandler = function(handler) {
+       window.console.log('Example of how to override deregisterScrollHandler.');
+       window.removeEventListener('scroll', handler, false);
+     };
+
+     bubble.showIfAllowed();
+   }, 1000);
+ }, false);
