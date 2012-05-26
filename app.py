b417f9e7f713bcb766d590e6a96af279ee028e7b
David Miller <David>
2012-05-26 15:55:47 +0100

    Adding libs and js test page

--- app.py
+++ app.py
@@ -4,11 +4,24 @@ from flask import render_template
 app = Flask(__name__)
 app.debug = True
 
+# Static includes
+import jinja2
 
+def include_file(name):
+    return jinja2.Markup(loader.get_source(env, name)[0])
+
+loader = jinja2.PackageLoader(__name__, 'templates')
+env = jinja2.Environment(loader=loader)
+env.globals['include_file'] = include_file
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/searching')
+def searching():
+    return env.get_template('search.html').render()
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
