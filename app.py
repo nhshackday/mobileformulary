0b47de1c1ce8666220fa0b7c18dacf31a6a2b22b
Jonathan Gillham <jonathan.gillham@qed4.com>
2012-05-28 22:28:45 +0100

    Removed some of the unicode characters from the bnf.json file.

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def drugs_like_me(term):
             break
 
     if splitter:
-        frist, rest= term.split(splitter, 1)
+        frist, rest = term.split(splitter, 1)
         results = drugs_like_me(frist)
         results += drugs_like_me(rest)
         return results
@@ -111,5 +111,3 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
