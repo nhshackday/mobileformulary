531090f1e77b0b1b5f40e09230c1069713ab6db7
David Miller <David>
2012-05-29 09:17:01 +0100

    nonsense

--- app.py
+++ app.py
@@ -109,6 +109,8 @@ def api_side_effects():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
+
+
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
